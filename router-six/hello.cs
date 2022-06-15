6de935da3705be35820d2e6bfc116a6149416d55
code-say <sayanmandal8643@gmail.com>
2022-06-15 10:25:04 +0530

    react projects

---
+++ router-six/hello.cs
@@ -0,0 +1,8 @@
+
+class Program {
+    static void Main(string[] args)
+    {
+        Console.writeline("hello World");
+        Console.Readkey();
+    }
+}
---
+++ site-one-context/src/problem.js
@@ -0,0 +1,34 @@
+import React, {useState} from "react";
+
+
+const GrandChild = props => {
+  return(
+    <div>
+      <h3>GrandChild: </h3>
+      <Child brand={props.brand} />
+    </div>
+  )
+}
+
+//props drilling 
+
+const Child = props => {
+  return(
+    <div>
+      <h2>Child: {props.brand}</h2>
+    </div>
+  )
+}
+
+const App = () => {
+  const [brandName] = useState("Amazon");
+  return (
+    <div>
+      <h1>Hello</h1>
+      
+      <GrandChild brand={brandName} />
+    </div>
+  );
+}
+
+export default App;
